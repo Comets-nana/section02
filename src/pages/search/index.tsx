85774a61ace7f599166bfff4e8047ca907bf8628
NaHyeon Kim <kimnahyun1124@gmail.com>
2025-02-15 02:54:00 +0900

    ✨ Update : 예제 UI 화면 구현 실습

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,15 +1,25 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
 
-    const router = useRouter(); // Hook 호출
-    const {q} = router.query;
+    // const router = useRouter(); // Hook 호출
+    // const {q} = router.query;
 
-    console.log(router); // console에 출력된 router객체 확인
+    // console.log(router); // console에 출력된 router객체 확인
 
-    return <h1>Search Result for: {q}</h1>;
+    // return <h1>Search Result for: {q}</h1>;
+
+    return (
+        <div>
+            {books.map((book) => (
+                <BookItem key={book.id} {...book} />
+            ))}
+        </div>
+    );
 }
 
 Page.getLayout = (page: ReactNode) => {
